c73b1db66ba2561f68643df295f485e331d342b9
makssmeinart <m.meinarts10@gmail.com>
2022-02-03 17:27:40 +0200

    Changed: BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './f1-main/m1-ui/App';
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
