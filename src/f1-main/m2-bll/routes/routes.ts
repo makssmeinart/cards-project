1bf11fc758aac8fd88f9de8627a5d9f129d62129
makssmeinart <m.meinarts10@gmail.com>
2022-02-03 17:02:09 +0200

    Folder structure

---
+++ src/f1-main/m2-bll/routes/routes.ts
@@ -0,0 +1,9 @@
+export const routes = {
+    profile: "/profile",
+    login: "/login",
+    register: "/register",
+    recoverPassword: "/recover-password",
+    newPassword: "/new-password",
+    testPage: "/test-page",
+    pageNotFound: "404",
+}
