f0ed67e2dfee6d9daa9938e5f38388bc03142551
makssmeinart <m.meinarts10@gmail.com>
2022-02-05 16:17:48 +0200

    Added: addReducer: status

--- src/f1-main/m2-bll/reducers/appReducer/appReducer.ts
+++ src/f1-main/m2-bll/reducers/appReducer/appReducer.ts
@@ -1,33 +1,44 @@
 const initState: InitStateTypes = {
-  isLoggedIn: false,
-  errorMessage: null,
+    isLoggedIn: false,
+    errorMessage: null,
+    status: "idle",
 };
 
 export const appReducer = (
-  state = initState,
-  action: ActionTypes
+    state = initState,
+    action: ActionTypes
 ): InitStateTypes => {
-  switch (action.type) {
-    case "AUTH/LOGIN/SET-IS-LOGGED-IN":
-      return { ...state, isLoggedIn: action.value };
-    case "APP/ERROR/MESSAGE":
-      return { ...state, errorMessage: action.value };
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "AUTH/LOGIN/SET-IS-LOGGED-IN":
+            return {...state, isLoggedIn: action.value};
+        case "APP/ERROR/MESSAGE":
+            return {...state, errorMessage: action.value}
+        case "APP/STATUS/CHANGE-STATUS":
+            return {...state, status: action.value}
+        default:
+            return state;
+    }
 };
 
 export const setIsLoggedInAC = (value: boolean) =>
-    ({ type: "AUTH/LOGIN/SET-IS-LOGGED-IN", value } as const);
+    ({type: "AUTH/LOGIN/SET-IS-LOGGED-IN", value} as const);
 
 export const errorMessageAC = (value: string | null) =>
     ({type: "APP/ERROR/MESSAGE", value} as const);
 
+export const changeStatus = (value: pendingStatusType) =>
+    ({type: "APP/STATUS/CHANGE-STATUS", value} as const)
+
 // Types
-type ActionTypes = setIsLoggedInACTypes | errorMessageACTypes;
+type ActionTypes =
+    |setIsLoggedInACTypes
+    | errorMessageACTypes
+    | ReturnType<typeof changeStatus>
 type setIsLoggedInACTypes = ReturnType<typeof setIsLoggedInAC>;
 type errorMessageACTypes = ReturnType<typeof errorMessageAC>;
+type pendingStatusType = "idle" | "failed" | "completed" | "succedded"
 type InitStateTypes = {
-  isLoggedIn: boolean;
-  errorMessage: string | null;
+    isLoggedIn: boolean;
+    errorMessage: string | null;
+    status: pendingStatusType
 };
