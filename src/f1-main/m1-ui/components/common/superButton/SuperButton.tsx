624884ef2b351609ab6c5712f8c0372fe5dd80a0
makssmeinart <m.meinarts10@gmail.com>
2022-02-08 14:58:26 +0200

    Updated: Common components styles

--- src/f1-main/m1-ui/components/common/superButton/SuperButton.tsx
+++ src/f1-main/m1-ui/components/common/superButton/SuperButton.tsx
@@ -1,17 +1,19 @@
-import React, { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+import React, {ButtonHTMLAttributes, DetailedHTMLProps} from "react";
+import s from "./superButton.module.css"
 
-type DefaultButtonPropsType = DetailedHTMLProps<
-  ButtonHTMLAttributes<HTMLButtonElement>,
-  HTMLButtonElement
->;
+type DefaultButtonPropsType = DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement>;
 
 type SuperButtonPropsType = DefaultButtonPropsType & {
-  red?: boolean;
+    red?: boolean,
+    className?: string,
 };
 
 export const SuperButton: React.FC<SuperButtonPropsType> = ({
-  className,
-  ...restProps
-}) => {
-  return <button {...restProps} />;
+                                                                className,
+                                                                ...restProps
+                                                            }) => {
+    const moduleStyles = className ? className : ""
+
+    return <button className={s[moduleStyles]} {...restProps} />;
 };
--- src/f2-pages/auth/login/Login.tsx
+++ src/f2-pages/auth/login/Login.tsx
@@ -1,111 +1,100 @@
-import { useFormik } from "formik";
-import { useDispatch, useSelector } from "react-redux";
-import { SuperInputText } from "../../../f1-main/m1-ui/components/common/superInput/SuperInput";
-import { SuperButton } from "../../../f1-main/m1-ui/components/common/superButton/SuperButton";
-import { SuperCheckbox } from "../../../f1-main/m1-ui/components/common/superCheckbox/SuperCheckbox";
-import { LoginParamsType } from "../../../f1-main/m3-dal/api";
-import { LoginTC } from "../../../f1-main/m2-bll/reducers/login/loginReducer";
-import { Link, Navigate } from "react-router-dom";
-import React, { useState } from "react";
-import { RootAppStateType } from "../../../f1-main/m2-bll/store";
-import { PendingStatusType } from "../../../f1-main/m2-bll/reducers/appReducer/appReducer";
+import {useFormik} from "formik";
+import {useDispatch, useSelector} from "react-redux";
+import {SuperInputText} from "../../../f1-main/m1-ui/components/common/superInput/SuperInput";
+import {SuperButton} from "../../../f1-main/m1-ui/components/common/superButton/SuperButton";
+import {SuperCheckbox} from "../../../f1-main/m1-ui/components/common/superCheckbox/SuperCheckbox";
+import {LoginParamsType} from "../../../f1-main/m3-dal/api";
+import {LoginTC} from "../../../f1-main/m2-bll/reducers/login/loginReducer";
+import {Link, Navigate} from "react-router-dom";
+import React from "react";
+import {RootAppStateType} from "../../../f1-main/m2-bll/store";
+import {PendingStatusType} from "../../../f1-main/m2-bll/reducers/appReducer/appReducer";
 import s from "./Login.module.css";
-import { routes } from "../../../f1-main/m2-bll/routes/routes";
-import { ErrorInput } from "../../../f1-main/m1-ui/components/common/errorInput/ErrorInput";
-import { ErrorSnackbar } from "../../../f1-main/m1-ui/components/common/errorSnackbar/ErrorSnackbar";
+import {routes} from "../../../f1-main/m2-bll/routes/routes";
+import {ErrorInput} from "../../../f1-main/m1-ui/components/common/errorInput/ErrorInput";
+import {ErrorSnackbar} from "../../../f1-main/m1-ui/components/common/errorSnackbar/ErrorSnackbar";
+import {SuperPasswordInput} from "../../../f1-main/m1-ui/components/common/superPasswordInput/SuperPasswordInput";
+import {WhitePaper} from "../../../f1-main/m1-ui/components/common/whitePaper/WhitePaper";
 
 export const Login = () => {
-  const dispatch = useDispatch();
-  const [reverseType, setReverseType] = useState<"password" | "text">(
-    "password"
-  );
-  const isLoggedIn = useSelector<RootAppStateType>(
-    (state) => state.app.isLoggedIn
-  );
-  const errorMessage = useSelector<RootAppStateType>(
-    (state) => state.app.errorMessage
-  );
-  const status = useSelector<RootAppStateType, PendingStatusType>(
-    (state) => state.app.status
-  );
-  const onClickHandler = () => {
-    if (reverseType === "password") {
-      setReverseType("text");
-    } else if (reverseType === "text") {
-      setReverseType("password");
+    const dispatch = useDispatch();
+
+    const isLoggedIn = useSelector<RootAppStateType>(
+        (state) => state.app.isLoggedIn
+    );
+    const status = useSelector<RootAppStateType, PendingStatusType>(
+        (state) => state.app.status
+    );
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            rememberMe: false,
+        },
+        onSubmit: (values) => {
+            dispatch(LoginTC(values));
+            formik.resetForm();
+        },
+        validate: (values) => {
+            const errors: Partial<Pick<LoginParamsType, "email" | "password">> = {};
+            if (!values.email) {
+                errors.email = "Required";
+            } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+            ) {
+                errors.email = "Invalid email address";
+            }
+            if (!values.password) {
+                errors.password = "Required";
+            } else if (values.password.length < 5) {
+                errors.password = "Invalid password";
+            }
+            return errors;
+        },
+    });
+
+    if (isLoggedIn) {
+        return <Navigate to={routes.profile}/>;
     }
-  };
 
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-      rememberMe: false,
-    },
-    onSubmit: (values) => {
-      dispatch(LoginTC(values));
-      formik.resetForm();
-    },
-    validate: (values) => {
-      const errors: Partial<Omit<LoginParamsType, "captcha">> = {};
-      if (!values.email) {
-        errors.email = "Required";
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-      ) {
-        errors.email = "Invalid email address";
-      }
-      if (!values.password) {
-        errors.password = "Required";
-      } else if (values.password.length < 5) {
-        errors.email = "Invalid password";
-      }
-      return errors;
-    },
-  });
+    return (
+        <>
+            {status === "loading" ? (
+                <div>loading...</div>
+            ) : (
+                <section className={s.s}>
+                    <WhitePaper>
+                        <form className={s.s} onSubmit={formik.handleSubmit}>
+                            <SuperInputText labelValue={"Email"} type={"text"} {...formik.getFieldProps("email")} />
+                            {formik.touched.email && formik.errors.email ? (
+                                <ErrorInput error={formik.errors.email}/>
+                            ) : null}
+                            <SuperPasswordInput
+                                value={formik.getFieldProps("password").value}
+                                onChange={formik.getFieldProps("password").onChange}
+                                name={formik.getFieldProps("password").name}
+                                onBlur={formik.getFieldProps("password").onBlur}
+                            />
+                            {formik.touched.password && formik.errors.password ? (
+                                <ErrorInput error={formik.errors.password}/>
+                            ) : null}
+                            <SuperCheckbox
+                                name="rememberMe"
+                                onChange={formik.handleChange}
+                                checked={formik.values.rememberMe}
+                            />
+                            <SuperButton className={"primaryButton"} type={"submit"}>Login</SuperButton>
+                        </form>
 
-  if (isLoggedIn) {
-    return <Navigate to={routes.profile} />;
-  }
+                        <Link to={"/register"}>Register</Link>
 
-  return (
-    <>
-      {status === "loading" ? (
-        <div>loading...</div>
-      ) : (
-        <section className={s.s}>
-          <form className={s.s} onSubmit={formik.handleSubmit}>
-            <SuperInputText type={"text"} {...formik.getFieldProps("email")} />
-            {formik.touched.email && formik.errors.email ? (
-              // <div>{formik.errors.email}</div>
-              <ErrorInput error={formik.errors.email} />
-            ) : null}
-            <SuperInputText
-              type={reverseType}
-              {...formik.getFieldProps("password")}
-            />
-            {formik.touched.password && formik.errors.password ? (
-              // <div>{formik.errors.password}</div>
-              <ErrorInput error={formik.errors.password} />
-            ) : null}
-            <SuperButton type={"button"} onClick={onClickHandler}>
-              hide
-            </SuperButton>
-            <SuperCheckbox
-              name="rememberMe"
-              onChange={formik.handleChange}
-              checked={formik.values.rememberMe}
-            />
-            <SuperButton type={"submit"}>Login</SuperButton>
-          </form>
-          {errorMessage ? <div>.{errorMessage}.</div> : null}
-
-          <Link to={"/register"}>Register</Link>
-
-          {/* Error Snackbar */}
-          <ErrorSnackbar />
-        </section>
-      )}
-    </>
-  );
+                        {/* Error Snackbar */}
+                        <ErrorSnackbar/>
+                    </WhitePaper>
+                </section>
+            )}
+        </>
+    );
 };
