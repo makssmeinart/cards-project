f86577e4798323fee13a22c92c9e248c42075786
Aliaksandr Rudyk <dezo103@gmail.com>
2022-02-09 12:56:31 +0300

    add redirect if success, add catch, finally

--- src/f1-main/m2-bll/reducers/register/registerReducer.ts
+++ src/f1-main/m2-bll/reducers/register/registerReducer.ts
@@ -1,28 +1,40 @@
 import {Dispatch} from "redux";
 import {authApi} from "../../../m3-dal/api";
+import {changeStatus, setIsLoggedInAC} from "../appReducer/appReducer";
+import {serverErrorHandling} from "../../../m4-utility/serverErrorHandling";
 
 const initState: InitStateType = {}
 
 export const registerReducer = (state = initState, action: ActionType): InitStateType => {
-    switch(action.type) {
+    switch (action.type) {
         case "AUTH/REGISTER": {
             return {...state}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
 // Action Creators
 
 export const RegisterAC = (data: any) => {
-    return { type: "AUTH/REGISTER", data } as const
+    return {type: "AUTH/REGISTER", data} as const
 }
 
 // Thunk
 
-export const RegisterTC = (data: any) => (dispatch: Dispatch) => {
-    authApi.register(data).then((res)=>{
-        console.log(res)})
+export const RegisterTC = (data: any, setIsSuccessRegister: any) => (dispatch: Dispatch) => {
+    dispatch(changeStatus("loading"))
+    authApi.register(data).then((res) => {
+        dispatch(changeStatus("completed"))
+        dispatch(setIsLoggedInAC(false))
+        setIsSuccessRegister(true)
+    }).catch((e) => {
+        dispatch(changeStatus('failed'))
+        serverErrorHandling(e, dispatch)
+    }).finally(()=> {
+        dispatch(changeStatus("idle"))
+    })
 }
 
 // Types
--- src/f2-pages/auth/register/Register.tsx
+++ src/f2-pages/auth/register/Register.tsx
@@ -3,10 +3,23 @@ import {ErrorSnackbar} from "../../../f1-main/m1-ui/components/common/errorSnack
 import wpStyle from "../../../f1-main/m1-ui/components/common/whitePaper/whitePapter.module.css"
 import {SuperInputText} from "../../../f1-main/m1-ui/components/common/superInput/SuperInput";
 import {SuperPasswordInput} from "../../../f1-main/m1-ui/components/common/superPasswordInput/SuperPasswordInput";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {RegisterTC} from "../../../f1-main/m2-bll/reducers/register/registerReducer";
+import {Navigate} from "react-router-dom";
+import {routes} from "../../../f1-main/m2-bll/routes/routes";
+import React, {useState} from "react";
+import {RootAppStateType} from "../../../f1-main/m2-bll/store";
 
 export const Register = () => {
+
+    // const error = useSelector<RootAppStateType, string | null>(
+    //     (state) => state.app.errorMessage
+    // );
+    // const isLoggedIn = useSelector<RootAppStateType>(
+    //     (state) => state.app.isLoggedIn
+    // );
+
+    const [isSuccessRegister, setIsSuccessRegister] = useState<boolean>(false)
 
     const data = {
         email: "nya-admin@nya.nya",
@@ -18,12 +31,22 @@ export const Register = () => {
         password: "1qazxcsdgsdgsdvBG"
     }
 
+    const data3 = {
+        email: "adminsrsgfgfgs@nya.nya",
+        password: "1qazxcsdgfgfgsdgsdvBG"
+    }
+
 
     const dispatch = useDispatch();
     const onclickHandler = () => {
-        dispatch(RegisterTC(data2)
+        dispatch(RegisterTC(data3, setIsSuccessRegister)
         )
     }
+
+    if (isSuccessRegister) {
+        return <Navigate to={routes.login} />;
+    }
+
 
     return (
         <section>
@@ -36,8 +59,8 @@ export const Register = () => {
                     <SuperInputText labelValue={"Email"}/>
                     <SuperPasswordInput/>
                 </div>
-                <ErrorSnackbar/>
             </WhitePaper>
+            <ErrorSnackbar/>
         </section>
     )
 }
