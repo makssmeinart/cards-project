2ba179fe42793aa486fdb97e2caeb565b85126ae
kontrast7 <maks.mashko@gmail.com>
2022-02-10 16:50:34 +0300

    add Card

--- src/f1-main/m1-ui/components/common/whitePaper/WhitePaper.tsx
+++ src/f1-main/m1-ui/components/common/whitePaper/WhitePaper.tsx
@@ -1,17 +1,25 @@
 import s from "./whitePapter.module.css"
 import {ReactNode} from "react";
+//@ts-ignore
+import Tilt from 'react-tilt'
+
+
 
 export const WhitePaper = (props: WhitePaperPropsType) => {
     return (
-        <div className={s.wrapper}>
-            <div className={s.inner}>
-                <h1 className={s.title}>
-                    It-incubator
-                </h1>
-                <div className={s.content}>
-                    {props.children}
+        <div className={s.container}>
+        <Tilt className="Tilt" options={{max: 10, scale: 1.03}} style={{maxWidth: "400px", width: "100%",backdropFilter: "blur(5px)"}}>
+            <div className={s.wrapper}>
+                <div className={s.inner}>
+                    <h1 className={s.title}>
+                        It-incubator
+                    </h1>
+                    <div className={s.content}>
+                        {props.children}
+                    </div>
                 </div>
             </div>
+        </Tilt>
         </div>
     )
 }
