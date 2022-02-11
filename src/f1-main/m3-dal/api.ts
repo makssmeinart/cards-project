058f1f37e0dbc3bccb1aafc74514325fda9e92ea
makssmeinart <m.meinarts10@gmail.com>
2022-02-11 17:17:11 +0200

    Updated: Button hover style

--- src/f1-main/m3-dal/api.ts
+++ src/f1-main/m3-dal/api.ts
@@ -4,7 +4,7 @@ import { InitStateType } from "../m2-bll/reducers/login/loginReducer";
 
 // Axios Instance
 const instance = axios.create({
-  baseURL: "http://localhost:7542/2.0/",
+  baseURL: "https://neko-back.herokuapp.com/2.0",
   withCredentials: true,
 });
 
