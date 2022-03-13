c33a0513d22dc6227d7db8a8cb1992a52654a927
kontrast7 <maks.mashko@gmail.com>
2022-03-13 16:25:14 +0300

    add forum branch add, forum branch delete

--- src/f2-pages/forum/Forum.tsx
+++ src/f2-pages/forum/Forum.tsx
@@ -1,52 +1,92 @@
-import { Header } from "../../f1-main/m1-ui/components/common";
-import { collection} from "firebase/firestore";
+import {
+  Header,
+  Loading,
+  SuperButton,
+  SuperInputText,
+} from "../../f1-main/m1-ui/components/common";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "../../f1-main/m3-dal/firebase/config";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getAllForumBranchesTC } from "../../f1-main/m2-bll/reducers/forumReducer/forumReducer";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAllForums } from "../../f1-main/m2-bll/selectors/selectAppStatus";
 import { Article } from "./components/Article";
+import s from "./Forum.module.css";
+import { RootAppStateType } from "../../f1-main/m2-bll/store";
+import { PendingStatusType } from "../../f1-main/m2-bll/reducers/appReducer/appReducer";
+import { Navigate } from "react-router-dom";
+import { routes } from "../../f1-main/m2-bll/routes/routes";
 
 export const Forum = () => {
+  const [newChatValue, setNewChatValue] = useState("");
   const colRef = collection(db, "branches");
-
   const dispatch = useDispatch();
+  const allForums = useSelector(selectAllForums);
+  const status = useSelector<RootAppStateType, PendingStatusType>(
+    (state) => state.app.status
+  );
+  const isLoggedIn = useSelector<RootAppStateType>(
+    (state) => state.app.isLoggedIn
+  );
+
+  const email = useSelector<RootAppStateType>((state) => state.login.email);
 
   useEffect(() => {
     dispatch(getAllForumBranchesTC(colRef));
   }, []);
 
-  const allForums = useSelector(selectAllForums);
+  const addToServer = () => {
+    let time = new Date().getTime()
 
-  // const addToServer = () => {
-  //   addDoc(colRef, {
-  //     chat: {
-  //       name: newChatName,
-  //       data: [
-  //         {
-  //           createdData: Date.now(),
-  //           message: "New ting2",
-  //           userName: "kiril2",
-  //         },
-  //       ],
-  //     },
-  //   }).then((res) => console.log(res));
-  // };
 
-  // const deleteToServer = (id: string) => {
-  //   const docRef = doc(db, "branches", id);
-  //   deleteDoc(docRef);
-  // };
+    addDoc(colRef, {
+      chat: {
+        name: newChatValue,
+        isAdmin: email,
+        data: [
+          {
+            createdData: time,
+            message: "New ting2",
+            userName: "kiril2",
+          },
+        ],
+      },
+    }).then((res) => {
+      setNewChatValue("");
+      dispatch(getAllForumBranchesTC(colRef));
+    });
+  };
+
+  if (!isLoggedIn) {
+    return <Navigate to={routes.login} />;
+  }
 
   return (
-    <div>
-      <Header />
-      <div>
-        <h1>Forum will be here</h1>
-        {allForums.map((article) => {
-          return <Article key={article.id} article={article} />;
-        })}
-      </div>
-    </div>
+    <>
+      {status === "loading" ? (
+        <Loading />
+      ) : (
+        <div>
+          <Header />
+          <div className={s.container}>
+            <h1>Forums:</h1>
+            <div className={s.formsWrapper}>
+              <SuperInputText
+                placeholder={"Enter new Forum name"}
+                value={newChatValue}
+                onChange={(e) => setNewChatValue(e.target.value)}
+              />
+              <SuperButton className={"primaryButton"} onClick={addToServer}>
+                Add
+              </SuperButton>
+            </div>
+
+            {allForums.map((article) => {
+              return <Article key={article.id} article={article} />;
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
--- src/f2-pages/forum/components/Article.tsx
+++ src/f2-pages/forum/components/Article.tsx
@@ -1,16 +1,72 @@
-import { ForumType } from "../../../f1-main/m2-bll/reducers/forumReducer/forumReducer";
+import {
+  ForumType,
+  getAllForumBranchesTC,
+} from "../../../f1-main/m2-bll/reducers/forumReducer/forumReducer";
 import { NavLink } from "react-router-dom";
+import { db } from "../../../f1-main/m3-dal/firebase/config";
+import { doc, deleteDoc, collection } from "firebase/firestore";
+import { useDispatch, useSelector } from "react-redux";
+import s from "./Article.module.css";
+import { SuperButton } from "../../../f1-main/m1-ui/components/common";
+import { RootAppStateType } from "../../../f1-main/m2-bll/store";
 
 export const Article = ({ article }: ArticlePropsType) => {
+  const dispatch = useDispatch();
+  const colRef = collection(db, "branches");
+  const deleteToServer = (id: string) => {
+    const docRef = doc(db, "branches", id);
+    deleteDoc(docRef).then(() => {
+      dispatch(getAllForumBranchesTC(colRef));
+    });
+  };
+
+  const email = useSelector<RootAppStateType>((state) => state.login.email);
+
+  const lastMessage =
+    article.chat.data.length &&
+    article.chat.data[article.chat.data.length - 1].message;
+  const lastMessageAuthor =
+    article.chat.data.length &&
+    article.chat.data[article.chat.data.length - 1].userName;
+  const today =
+    article.chat.data.length &&
+    Number(article.chat.data[article.chat.data.length - 1].createdData);
+
+  const date = new Date(today)
+    .toISOString()
+    .replace("-", "/")
+    .split("T")[0]
+    .replace("-", "/");
+
   return (
-    <NavLink to={`/forum/${article.id}`}>
-      <div>
-        <div>ava</div>
-        <div>
-          <h2>{article.chat.name}</h2>
+    <div className={s.container}>
+      <div className={s.inner}>
+        <div className={s.content}>
+          <h2 className={s.title}>
+            <NavLink to={`/forum/${article.id}`}>{article.chat.name}</NavLink>
+          </h2>
+
+          <div className={s.lastMessageContainer}>
+            <p>{lastMessage}</p>
+            <div className={s.dateAndAuthor}>
+              <p>{lastMessageAuthor}</p>
+              <p>{date}</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={s.buttonsWrapper}>
+          {email === article.chat.isAdmin ? (
+            <SuperButton
+              className={"secondaryButton"}
+              onClick={() => deleteToServer(article.id)}
+            >
+              Delete
+            </SuperButton>
+          ) : null}
         </div>
       </div>
-    </NavLink>
+    </div>
   );
 };
 
