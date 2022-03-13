c931bdb78727e30e7bf20fdc8503911a3a5f4a67
kontrast7 <maks.mashko@gmail.com>
2022-03-13 17:32:39 +0300

    refactor styles

--- src/f2-pages/forum/components/articlePage/ArticlePage.tsx
+++ src/f2-pages/forum/components/articlePage/ArticlePage.tsx
@@ -1,14 +1,16 @@
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import { getAllForumBranchesTC } from "../../../../f1-main/m2-bll/reducers/forumReducer/forumReducer";
 import { useDispatch, useSelector } from "react-redux";
-import { collection } from "firebase/firestore";
+import {addDoc, collection} from "firebase/firestore";
 import { db } from "../../../../f1-main/m3-dal/firebase/config";
 import { selectAllForums } from "../../../../f1-main/m2-bll/selectors/selectAppStatus";
 import { useParams } from "react-router-dom";
 import s from "./ArticlePage.module.css";
 import {Header} from "../../../../f1-main/m1-ui/components/common";
+import {RootAppStateType} from "../../../../f1-main/m2-bll/store";
 
 export const ArticlePage = () => {
+  const [inputValue, setInputValue] = useState("")
   const colRef = collection(db, "branches");
   const dispatch = useDispatch();
   const { forumId } = useParams();
@@ -22,6 +24,30 @@ export const ArticlePage = () => {
   }, []);
 
   console.log(forum);
+  const email = useSelector<RootAppStateType>((state) => state.login.email);
+
+
+
+  const addToServer = () => {
+    let time = new Date().getTime();
+
+    addDoc(colRef, {
+      chat: {
+        name: forum[0].chat.name,
+        isAdmin: email,
+        data: [
+          {
+            createdData: time,
+            message: inputValue,
+            userName: email,
+          },
+        ],
+      },
+    }).then((res) => {
+      setInputValue("");
+      dispatch(getAllForumBranchesTC(colRef));
+    });
+  };
 
   return (
     <section>
@@ -30,6 +56,9 @@ export const ArticlePage = () => {
         forum[0].chat.data.map((m, i) => {
           return <div key={Math.random() * i}>{m.message}</div>;
         })}
+
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+      <button onClick={addToServer}>send</button>
     </section>
   );
 };
